e83146590ea1247571743c097fe1895c962522aa
くらりゅ！ <s1f102501260@iniad.org>
2025-07-29 12:42:15 +0900

    addcontent

--- section13/class02.js
+++ section13/class02.js
@@ -11,6 +11,14 @@ class Item {
         this.stock -= cnt;
     }
 }
+class food extends Item {
+    constructor(name, stock, price, limitDate){
+        super(name, stock);
+        this.price = price;
+        this.limitDate = limitDate;
+    }
+}
 
 let peach = new Item('ピーチ', 10);
 let banana = new Item('バナナ', 10);
+let apple = new food('リンゴ', 10, 100);
