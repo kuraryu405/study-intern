37ad1cd76aa961f23795f3dbc6b3d924419e9069
くらりゅ！ <s1f102501260@iniad.org>
2025-08-18 12:37:45 +0900

    難しいね

--- JS,CSS.Sass/05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ JS,CSS.Sass/05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -1,7 +1,21 @@
 const child = document.querySelector('.child')
-const cb = function(){
-    alert('interesting')
+const cb = function(entries, observer){
+    console.log('IntersectionObserver triggered!', entries);
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            console.log('Element is visible!');
+            entry.target.style.backgroundColor = 'red';
+        }else{
+            console.log('Element is hidden!');
+            entry.target.style.backgroundColor = 'blue';
+        }
+    });
 }
-const io = new IntersectionObserber(cb);
+const options = {
+    root: null,
+    rootMargin: '-400px 0px -400px 0px',
+    threshold: 0,//0~1の間で指定する。0は画面に入った時に実行、1は画面に入った時に実行。
+}
+const io = new IntersectionObserver(cb, options);
 
 io.observe(child);
