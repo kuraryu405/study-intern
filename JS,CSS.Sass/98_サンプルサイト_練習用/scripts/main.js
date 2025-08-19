0a5fe5544179d800d69b48e997138f790c6f36ca
くらりゅ！ <s1f102501260@iniad.org>
2025-08-19 11:26:45 +0900

    難しい

--- JS,CSS.Sass/98_サンプルサイト_練習用/scripts/main.js
+++ JS,CSS.Sass/98_サンプルサイト_練習用/scripts/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hero = new HeroSlider('.swiper');
     hero.start();
+
+    const ta = new TweenTextAnimation('.tween-animate-title')
+    ta.animate();
+
+    const cb = function(el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+
 });
