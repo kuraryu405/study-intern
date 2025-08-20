8867d912d2a5a2c69fae9f2a4c395096005eadf5
くらりゅ！ <s1f102501260@iniad.org>
2025-08-20 16:24:42 +0900

    ほぼ書き直し

--- Atcoder/abc417/b/main.js
+++ Atcoder/abc417/b/main.js
@@ -1,21 +1,13 @@
 let input = require('fs').readFileSync('/dev/stdin', 'utf8');
-let input_list = input.trim().split('\n');
-// console.log(input_list);
-let NM_list = input_list[0].split(' ');
-let M = NM_list[1];
-let A_list = input_list[1].split(' ');
-// console.log(NM_list)
-// console.log(A_list)
-let B_list = input_list[2].split(' ');
-let result = []
-for (let i = 0; i < M; i++){
-    for(j of B_list){
-        for(k of A_list){
-            if(k == j){
-                A_list = A_list.filter(x => x != k);
-                break;
-            }
-        }
-    }
+let lines = input.trim().split('\n');
+let A = lines[1].split(' ');
+let B = lines[2].split(' ');
+
+for (const b of B) {
+  const idx = A.indexOf(b);
+  if (idx !== -1) {
+    A.splice(idx, 1);  
+  }
 }
-console.log(A_list)
+
+console.log(A.join(' '));
