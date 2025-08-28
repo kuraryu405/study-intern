5b7a56aeb1d2284434e0df0196554123f5c391fa
くらりゅ！ <s1f102501260@iniad.org>
2025-08-28 15:46:57 +0900

    kurusii

---
+++ React/reactlean/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { App } from './app';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
