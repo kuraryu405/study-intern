a4562fcded108349ff96b7ca4dec83e13999bede
くらりゅ！ <s1f102501260@iniad.org>
2025-08-29 11:38:59 +0900

    React-todo環境構築

--- React/reactlean/app.jsx
+++ React/reactlean/app.jsx
@@ -1,8 +1,10 @@
 export const App = () => {
+    const onClickButton = () => alert();
     return (
       <>
         <h1>こんにちは！</h1>
         <p>お元気ですか？</p>
+        <button onClick={() => alert(hogehuga)}>ボタン</button>
       </>
     );
   };
