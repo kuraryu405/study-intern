6740112cf33dd20f726ea69639692924aad1994e
くらりゅ！ <s1f102501260@iniad.org>
2025-08-30 22:56:12 +0900

    Update AddTodo.jsx

--- Todo/src/AddTodo.jsx
+++ Todo/src/AddTodo.jsx
@@ -7,10 +7,15 @@ import './App.css'
 export default function AddTodo() {
 
     return (
+        <>
         <div className="text-left">
-            <input type="text" className='w-500 m-8 ml-1 h-16 p-5 rounded-xl' placeholder="ここに入力"/>
-            <button className="Neumorphism-button">Add</button>
+            <input type="text"  className='w-500 m-8 ml-1 h-16 p-5 rounded-xl' placeholder="ここに入力" />
+            <button className="Neumorphism-button" >Add</button>
         </div>        
-
+        <button className='Neumorphism-button-del'>del</button>
+        </>
+        
     );
   }
+//   value={todo.title}
+// onChange={(e) => setTodo({...todo, title: e.target.value})}
