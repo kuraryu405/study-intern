c673e8f2e1b9d322e0c65d2343e279de7a5156ee
くらりゅ！ <s1f102501260@iniad.org>
2025-09-01 12:35:09 +0900

    完成！

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -4,16 +4,43 @@ import viteLogo from '/vite.svg'
 import React from 'react'
 import './App.css'
 import AddTodo from './AddTodo'
+import InComplete from './InComplete'
+import InProgress from './InProgress'
+import Complete from './Complete'
 
 function App() {
   // ここにtodoの状態を管理するものを入れる
+  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState({ title: '', status: '', due: '' });
 
+ 
   function addTodo(title, status, due) {
-    // TODO: 実際のtodoリストに追加する処理を実装
+    const incompleteCount = todos.filter(todo => todo.status === 'InComplete').length;
+    
+    if (incompleteCount >= 3) {
+      alert('タスクの上限が来ました');
+      return; 
+    }
+  
+    if (title.trim() === '') {
+      alert('タイトルを入力してください');
+      return;
+    }
+  
     console.log('Adding todo:', { title, status, due });
-
+    setTodos([...todos, { id: Date.now(), title, status, due }]);
     setTodo({ title: '', status: '', due: '' });
+  }
+
+  function moveInProgressTodo(id) {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, status: 'InProgress' } : todo));
+  }
+
+  function moveCompleteTodo(id) {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, status: 'Complete' } : todo));
+  }
+  function deleteTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
   }
   
   return (
@@ -21,34 +48,11 @@ function App() {
       {/* これはtodo追加 */}
 
     <AddTodo todo={todo} setTodo={setTodo} addTodo={addTodo} />
+    <InComplete todos={todos} setTodos={setTodos} moveInProgressTodo={moveInProgressTodo} deleteTodo={deleteTodo} />
+    <InProgress todos={todos} setTodos={setTodos} moveCompleteTodo={moveCompleteTodo} deleteTodo={deleteTodo} />
+    <Complete todos={todos} setTodos={setTodos} deleteTodo={deleteTodo} />
     {/* ここより下は後でコンポーネント化する */}
-    <div className="Incomplete mb-10">
-      <p className="text-2xl font-black text-left mb-3">InComplete</p>
-      <div className=" w-[100%] h-[254px] rounded-[30px] bg-[#e0e0e0]"
-          style={{
-                  boxShadow: `15px 15px 30px #bebebe, -15px -15px 30px #ffffff`
-                }}>
-          {/* tasklist */}
-      </div>
-    </div>
-    <div className="Inprogress mb-10">
-    <p className="text-2xl font-black text-left mb-3">InProgress</p>
-      <div className=" w-[100%] h-[254px] rounded-[30px] bg-[#e0e0e0]"
-          style={{
-                  boxShadow: `15px 15px 30px #bebebe, -15px -15px 30px #ffffff`
-                }}>
-          {/* tasklist */}
-      </div>
-    </div>
-    <div className="complete">
-    <p className="text-2xl font-black text-left mb-3">Complete</p>
-      <div className=" w-[100%] h-[254px] rounded-[30px] bg-[#e0e0e0]"
-          style={{
-                  boxShadow: `15px 15px 30px #bebebe, -15px -15px 30px #ffffff`
-                }}>
-          {/* tasklist */}
-      </div>
-    </div>
+    
 
     </>
   )
---
+++ Todo/src/InComplete.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function InComplete({ todos, setTodos, moveInProgressTodo, deleteTodo }) {
+    return (
+        <>
+            <div className="Incomplete mb-10">
+                <p className="text-2xl font-black text-left mb-3">InComplete</p>
+                <div
+                    className="w-[100%] h-[254px] rounded-[30px] bg-[#e0e0e0]"
+                    style={{
+                        boxShadow: `15px 15px 30px #bebebe, -15px -15px 30px #ffffff`
+                    }}
+                >
+                    {/* tasklist */}
+                    {todos
+                        .filter(todo => todo.status === 'InComplete')
+                        .map(todo => (
+                            <li
+                                key={todo.id}
+                                className="p-2 m-2 rounded-xl text-left pl-8 pt-4"
+                            >
+                                {todo.title}
+                                <button className="Neumorphism-button mr-6 ml-6" onClick={() => moveInProgressTodo(todo.id)}>
+                                    InProgress
+                                </button>
+                                <button className="Neumorphism-button-del" onClick={() => deleteTodo(todo.id)}>
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                </div>
+            </div>
+        </>
+    );
+}
