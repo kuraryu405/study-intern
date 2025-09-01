50943b4b3d5f875fae8c5db75fe28e0365b95b53
くらりゅ！ <s1f102501260@iniad.org>
2025-09-01 15:52:41 +0900

    typescript

---
+++ TypeScript/script.ts
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+button?.addEventListener("click", () => {
+    console.log("ボタンがクリックされました");
+});
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(sum(1, 2));
