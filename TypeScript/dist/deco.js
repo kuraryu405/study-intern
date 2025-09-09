1d746bb6eae57e421ada1ddc8b0d7ab5641528c2
くらりゅ！ <s1f102501260@iniad.org>
2025-09-09 12:49:08 +0900

    add ts

--- TypeScript/dist/deco.js
+++ TypeScript/dist/deco.js
@@ -1,21 +1,49 @@
 "use strict";
+// function Logger(logString: string){
+//     return function(constructor: Function){
+//         console.log(logString);
+//         console.log(constructor);
+//     }
 Object.defineProperty(exports, "__esModule", { value: true });
-function Logger(logString) {
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
-}
-@Logger('logを出力します')
-class Person {
-    name;
-    age;
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-        console.log('Personをインスタンス化しました');
+// }
+// function WithTemplate(template: string, hookId: string){
+//     return function(constructor: Function){
+//         const hookEl = document.getElementById(hookId);
+//         const p = new constructor();
+//         if (hookEl){
+//             hookEl.innerHTML = template;
+//         }
+//     }
+// }
+// @Logger('logを出力します')
+// class Person{
+//     name: string;
+//     age: number;
+//     constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//         console.log('Personをインスタンス化しました');
+//     }
+// }
+// const person = new Person('John', 30);
+// console.log(person);
+class Product {
+    title;
+    _price;
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('0以下やんけ');
+        }
+    }
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    getPriceWithTax(tax) {
+        return this.price * (1 + tax);
     }
 }
-const person = new Person('John', 30);
-console.log(person);
 //# sourceMappingURL=deco.js.map
--- TypeScript/src/deco.ts
+++ TypeScript/src/deco.ts
@@ -1,22 +1,54 @@
-function Logger(logString: string){
-    return function(constructor: Function){
-        console.log(logString);
-        console.log(constructor);
+// function Logger(logString: string){
+//     return function(constructor: Function){
+//         console.log(logString);
+//         console.log(constructor);
+//     }
+
+// }
+
+// function WithTemplate(template: string, hookId: string){
+//     return function(constructor: Function){
+//         const hookEl = document.getElementById(hookId);
+//         const p = new constructor();
+//         if (hookEl){
+//             hookEl.innerHTML = template;
+//         }
+//     }
+// }
+
+
+// @Logger('logを出力します')
+// class Person{
+//     name: string;
+//     age: number;
+
+//     constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//         console.log('Personをインスタンス化しました');
+//     }
+// }
+
+// const person = new Person('John', 30);
+// console.log(person);
+
+
+class Product{
+    title: string;
+    private _price: number;
+
+    set price(val: number){
+        if (val > 0){
+            this._price = val;
+        } else {
+            throw new Error('0以下やんけ');
+        }
     }
-
-}
-
-@Logger('logを出力します')
-class Person{
-    name: string;
-    age: number;
-
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
-        console.log('Personをインスタンス化しました');
+    constructor(title: string, price: number){
+        this.title = title;
+        this.price = price;
+    }
+    getPriceWithTax(tax: number): number{
+        return this.price * (1 + tax);
     }
 }
-
-const person = new Person('John', 30);
-console.log(person);
