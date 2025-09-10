99e0a6822a84ddab47122d6b6fa8f1c1af9802e3
くらりゅ！ <s1f102501260@iniad.org>
2025-09-10 11:15:16 +0900

    add content

--- survival-TS/script.ts
+++ survival-TS/script.ts
@@ -58,3 +58,31 @@ switch (color) {
   default:
     console.log("Color is neither red nor blue.");
 }
+
+class Circle {
+    private _radius: number;
+
+    constructor(radius: number) {
+      this._radius = radius;
+    }
+
+    // ゲッター
+    get radius(): number {
+      return this._radius;
+    } 
+
+    // セッター
+    set radius(radius: number) {
+      if (radius <= 0) {
+        throw new Error("Invalid radius value");
+      }
+      this._radius = radius;
+    }
+  }
+  
+  const circle = new Circle(5);
+  console.log(circle.radius);
+  circle.radius = 3;
+  console.log(circle.radius);
+  circle.radius = -2;
+  // 例外: 'Invalid radius value'
