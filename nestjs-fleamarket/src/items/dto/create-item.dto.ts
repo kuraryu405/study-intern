4eba94f178c00a083c42e89dd19e46d7e3f6a87b
くらりゅ！ <s1f102501260@iniad.org>
2025-09-17 17:21:37 +0900

    add vali

--- nestjs-fleamarket/src/items/dto/create-item.dto.ts
+++ nestjs-fleamarket/src/items/dto/create-item.dto.ts
@@ -1,6 +1,17 @@
+import { IsString, IsNotEmpty, MaxLength, IsNumber, Min, IsOptional } from 'class-validator';
 export class CreateItemDto {
-    id: string;
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(40)
     name: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1)
     price: number;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(1000)
     description?: string;
 }
