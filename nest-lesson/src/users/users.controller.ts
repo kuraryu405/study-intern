84bed3b5636b83be84002b2213dd9d8fdfb48822
くらりゅ！ <s1f102501260@iniad.org>
2025-09-30 12:37:12 +0900

    add create-user-dto.ts

--- nest-lesson/src/users/users.controller.ts
+++ nest-lesson/src/users/users.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Body, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Post()
+    create(@Body() CreateUser: CreateUserDto){
+        return CreateUser
+    }
+}
