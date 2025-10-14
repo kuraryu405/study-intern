6fd7a318f0216c3f43f3092c2812c4c7de0fd87c
くらりゅ！ <s1f102501260@iniad.org>
2025-10-14 12:09:25 +0900

    s

--- nestjs-fleamarket/src/auth/auth.module.ts
+++ nestjs-fleamarket/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthModule } from './dto/auth/auth.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [AuthModule],
 })
 export class AuthModule {}
